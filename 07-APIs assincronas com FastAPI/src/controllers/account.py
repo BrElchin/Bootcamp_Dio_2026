fb6479d5524a4f7722b35c954d1a3f0150280cea
BrElchin <bruno.elchin45@gmail.com>
2026-01-08 21:39:56 -0300

    Create account.py

---
+++ 07-APIs assincronas com FastAPI/src/controllers/account.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, status
+
+from src.schemas.account import AccountIn
+from src.security import login_required
+from src.services.account import AccountService
+from src.services.transaction import TransactionService
+from src.views.account import AccountOut, TransactionOut
+
+router = APIRouter(prefix="/accounts", dependencies=[Depends(login_required)])
+
+account_service = AccountService()
+tx_service = TransactionService()
+
+
+@router.get("/", response_model=list[AccountOut])
+async def read_accounts(limit: int = 10, skip: int = 0):
+    """Lista todas as contas com paginação."""
+    return await account_service.read_all(limit=limit, skip=skip)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=AccountOut)
+async def create_account(account: AccountIn):
+    """Cria uma nova conta corrente."""
+    return await account_service.create(account)
+
+
+@router.get("/{id}/transactions", response_model=list[TransactionOut])
+async def read_account_transactions(id: int, limit: int = 10, skip: int = 0):
+    """Lista as transações de uma conta específica com paginação."""
+    return await tx_service.read_all(account_id=id, limit=limit, skip=skip)
