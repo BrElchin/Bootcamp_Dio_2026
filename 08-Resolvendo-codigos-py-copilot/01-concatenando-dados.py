99a7a209f533ec03d7ffedf5e6a60707367c3546
BrElchin <bruno.elchin45@gmail.com>
2026-01-10 17:56:38 -0300

    Create 01-concatenando-dados.py

---
+++ 08-Resolvendo-codigos-py-copilot/01-concatenando-dados.py
@@ -0,0 +1,8 @@
+# 1 - Concatenando Dados 🐾
+# Receba dois dados diferentes do usuário e concatene em uma única string
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+mensagem = f"Olá, {nome}! Você tem {idade} anos."
+print(mensagem)
